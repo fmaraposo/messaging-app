273c6109cd9217db2fe07c99fdae6337eebfe430
Francisco Martins <fmaraposo@gmail.com>
2022-08-14 22:01:55 +0100

    Starting Building Channel Container

--- client/src/components/ChannelContainer.js
+++ client/src/components/ChannelContainer.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Channel, useChatContext } from "stream-chat-react";
+
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./";
 
 const ChannelContainer = () => {
+  const { channel } = useChatContext();
+
+  if (!channel) {
+  }
   return <div>Channel Container</div>;
 };
 
--- client/src/components/ChannelListContainer.js
+++ client/src/components/ChannelListContainer.js
@@ -5,7 +5,9 @@ import Cookies from "universal-cookie";
 import HospitalIcon from "../assets/hospital.png";
 import LogoutIcon from "../assets/logout.png";
 
-const SideBar = () => (
+const cookies = new Cookies()
+
+const SideBar = ({logout}) => (
   <div className="channel-list__sidebar">
     <div className="channel-list__sidebar__icon1">
       <div className="icon1__inner">
@@ -14,7 +16,7 @@ const SideBar = () => (
     </div>
     <div className="channel-list__sidebar__icon2">
       <div className="icon1__inner">
-        <img src={LogoutIcon} alt="logout" width={30} />
+        <img src={LogoutIcon} alt="logout" width={30} onClick={logout} />
       </div>
     </div>
   </div>
@@ -27,9 +29,20 @@ const CompanyHeader = () => (
 );
 
 const ChannelListContainer = () => {
+  const logout = () => {
+    cookies.remove("userId");
+    cookies.remove("username");
+    cookies.remove("fullName");
+    cookies.remove("avatarURL");
+    cookies.remove("phoneNumber");
+    cookies.remove("hashedPassword");
+    cookies.remove("token");
+
+    window.location.reload()
+  }
   return (
     <>
-      <SideBar />
+      <SideBar logout={logout}/>
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
         <ChannelSearch />
