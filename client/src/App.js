f33c1784bd9e8dfb587b8519ff2fd8d228540c3f
Francisco Martins <fmaraposo@gmail.com>
2023-04-07 11:07:51 +0100

    create express server

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,18 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import { ChannelContainer, ChannelListContainer } from "./components";
+import { ChannelContainer, ChannelListContainer, Auth} from "./components";
 import './App.css'
 
 const apiKey = "b5r5bpp6hvzj";
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+
+  if(!authToken) return <Auth />
+  
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth.js");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); // allows to pass JSON payloads from FE to BE
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use('/', authRoutes)
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
