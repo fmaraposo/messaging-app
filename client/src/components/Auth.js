47286abfe6849dd782b65df92d58bd02bc3a97e0
Francisco Martins <fmaraposo@gmail.com>
2023-04-11 12:08:42 +0100

    update url to correct api url

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -22,7 +22,7 @@ const Auth = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { username, password, phoneNumber, avatarURL } = form;
-    const URL = "http://localhost:5000/auth";
+    const URL = "https://messaging-app.cyclic.app/auth";
 
     const {
       data: { token, userId, hashedPassword, fullName},
