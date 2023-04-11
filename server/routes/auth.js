24cd9981d664fbe10d26102d760e69f881c1facd
Francisco Martins <fmaraposo@gmail.com>
2023-04-11 12:03:07 +0100

    push to deploy

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { signup, login } = require("../controllers/auth.js");
 
 router.post("/signup", signup);
+
 router.post("/login", login);
 
 module.exports = router;
